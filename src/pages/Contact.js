9640674d520fec7984e19ccb2d47ab3f175f1822
pawelsan <pawelsan@yahoo.com>
2019-08-29 21:12:15 +0200

    form works on netlify

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,32 @@
 import React from 'react';
+// import '../styles/ContactPage.css'
+// import { Prompt } from 'react-router-dom'
+
 const Contact = () => {
+    //     return (  );
+    // }
+
+
+
+    // class ContactPage extends React.Component {
+
+    // state = {
+    //     value: ''
+    // }
+
+    // handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     this.setState({
+    //         value: ''
+    //     })
+    // }
+
+    // handleChange = (e) => {
+    //     this.setState({
+    //         value: e.target.value
+    //     })
+    // }
+    // render() {
     return (
         <>
             <div class="container">
@@ -11,49 +38,77 @@ const Contact = () => {
                         <h1>Contact form </h1>
 
 
-                        <form name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true">
-                            <div class="messages"></div>
-                            <div class="controls">
-                                <div className="row">
-                                    <div className="col-md-6">
-                                        <div className="form-group">
-                                            <label htmlFor="form_name">Firstname *</label>
-                                            <input type="text" id="form_name" name="name" class="form-control" placeholder="Please enter your firstname *" required="required" data-error="Firstname is required" />
-                                            <div className="help-block with-errors"></div>
+                        <form name="contact" method="POST"
+                        // data-netlify="true"
+                        // onSubmit={this.handleSubmit}
+                        >
+                            <input type="hidden" name="form-name" value="contact" action="/" />
+                            <p>
+                                <label>Your Name: <input type="text" name="name" /></label>
+                            </p>
+                            <p>
+                                <label>Your Email: <input type="email" name="email" /></label>
+                            </p>
+                            <p>
+                                <label>Message: <textarea name="message"></textarea></label>
+                            </p>
+                            <p>
+                                <button type="submit">Send</button>
+                            </p>
+                            {/* <input type="hidden" name="form-name" value="the-name-of-the-html-form" />
+                            <h3>Napisz do nas</h3>
+                            <textarea name="message"
+                                // value={this.state.value}
+                                // onChange={this.handleChange} 
+                                placeholder={"Wpisz wiadomość"}>
+
+                            </textarea>
+                            <button type="submit">Wyślij</button> */}
+                            {/* <div class="messages"></div>
+                                <div class="controls">
+                                    <div className="row">
+                                        <div className="col-md-6">
+                                            <div className="form-group">
+                                                <label htmlFor="form_name">Firstname *</label>
+                                                <input type="text" id="form_name" name="name" class="form-control" placeholder="Please enter your firstname *" required="required" data-error="Firstname is required" />
+                                                <div className="help-block with-errors"></div>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-6">
+                                            <div className="form-group">
+                                                <label htmlFor="form_email">Email *</label>
+                                                <input type="text" id="form_email" name="email" class="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required" />
+                                                <div className="help-block with-errors"></div>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div className="col-md-6">
-                                        <div className="form-group">
-                                            <label htmlFor="form_email">Email *</label>
-                                            <input type="text" id="form_email" name="email" class="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required" />
-                                            <div className="help-block with-errors"></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col-md-12">
-                                        <div className="form-group">
-                                            <label htmlFor="form_message">Message *</label>
-                                            <textarea name="message" id="form_message" class="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please, leave us a message."></textarea>
-                                            <div className="help-block with-errors"></div>
-                                        </div>
-                                    </div>
-                                    <div className="field">
+                                    <div className="row">
+                                        <div className="col-md-12">
+                                            <div className="form-group">
+                                                <label htmlFor="form_message">Message *</label>
+                                                <textarea name="message" id="form_message" class="form-control" value={this.state.value} onChange={this.handleChange} placeholder="Wpisz wiadomość" rows="4" required="required" data-error="Please, leave us a message."></textarea>
+                                                <div className="help-block with-errors"></div>
+                                            </div>
+                                        </div> */}
+                            {/* <div className="field">
                                         <div data-netlify-recaptcha="true"></div>
+                                    </div> */}
+                            {/* <div className="col-md-12">
+                                            <input type="submit" class="btn btn-success btn-send" value="Send message" />
+                                        </div>
                                     </div>
-                                    <div className="col-md-12">
-                                        <input type="submit" class="btn btn-success btn-send" value="Send message" />
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col-md-12">
-                                        <p className="text-muted">
-                                            <strong>*</strong> These fields are required
+                                    <div className="row">
+                                        <div className="col-md-12">
+                                            <p className="text-muted">
+                                                <strong>*</strong> These fields are required
                                         </p>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
+                                </div> */}
                         </form>
+                        {/* <Prompt
+                            when={this.state.value}
+                            message="Formularz nie został wysłany. Opuszczenie strony spowoduje utratę wprowadzonych informacji" /> */}
 
                     </div>
 
@@ -63,6 +118,7 @@ const Contact = () => {
         </>
 
     );
+    // }
 }
-
 export default Contact;
+// export default ContactPage;
